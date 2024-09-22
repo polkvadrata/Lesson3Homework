700f83c38ff8f488f417ecf2115821afb54a41ac
polkvadrata <polkvadrata@gmail.com>
2024-09-22 13:45:16 +0300

    add console output

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] arg) {
 
         System.out.println("This is shop-online app");
+        System.out.println("Shop-online for customers");
 
     }
 
